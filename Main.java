8b141c79034dcba841f64e7dcfdefb3755e13a6a
Ayta <ayta88@list.ru>
2024-03-16 16:07:13 +0300

    main2

--- Main.java
+++ Main.java
@@ -3,16 +3,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        HotDrink coffe = new HotDrink("Coffe", 50, 150, 90);
-        HotDrink latte = new HotDrink("Latte", 70, 150, 90);
-        HotDrink cappuccino = new HotDrink("cappuccino", 60, 150, 80);
-        HotDrink espresso = new HotDrink("espresso", 55, 100, 85);
+        HotDrink coffe = new HotDrink("Raf", 200, 200, 90);
+        HotDrink latte = new HotDrink("Espresso", 150, 100, 80);
+        HotDrink cappuccino = new HotDrink("Cappuccino", 180, 200, 83);
+        HotDrink espresso = new HotDrink("Latte", 205, 200, 85);
 
         ArrayList<HotDrink> drinks = new ArrayList<>(List.of(coffe, latte, cappuccino, espresso));
 
         HotDrinkMachine avtomat = new HotDrinkMachine();
         avtomat.addDrink(drinks);
         System.out.println(avtomat.getProduct("Latte"));
-        System.out.println(avtomat.getProduct("espresso",  100, 85));
+        System.out.println(avtomat.getProduct("Espresso",  100, 80));
     }
 }
